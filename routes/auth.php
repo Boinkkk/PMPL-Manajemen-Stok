dae3bfda872f9b721a8a3bbc97d5979378f3466d
Boink <ivanroisussalam1234@gmail.com>
2026-05-20 04:50:09 +0700

    create middleware and stok, refactor route

---
+++ routes/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function (): void {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.proses');
+
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.proses');
+});
+
+Route::middleware(['auth', 'cek.status'])->group(function (): void {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
