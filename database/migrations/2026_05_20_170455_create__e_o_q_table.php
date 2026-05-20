181a85a2de3d080356351de7f611f30a3530f5b3
Boink <ivanroisussalam1234@gmail.com>
2026-05-21 00:15:51 +0700

    make eoq data

---
+++ database/migrations/2026_05_20_170455_create__e_o_q_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_eoq', function (Blueprint $table) {
+            $table->id("id_eoq");
+            $table->foreignId("id_produk")->nullable();
+            $table->bigInteger("permintaan_tahunan")->nullable();
+            $table->bigInteger("biaya_pemesanan")->nullable();
+            $table->bigInteger("biaya_penyimpanan")->nullable();
+            $table->bigInteger("eoq")->nullable();
+
+            $table->foreign('id_produk', 'fk_produk_eoq')->references('id_produk')->on('produk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_e_o_q');
+    }
+};
